333c6d41f8141151b2155fd2afd9e9f7e74eaeb9
Marçal Junior <marssaljr@gmail.com>
2024-05-07 15:38:48 -0300

    Add extension API call and message listener in main.js

--- main.js
+++ main.js
@@ -29,4 +29,20 @@ function runApp() {
       'height': 768
     }
   });
+
+  const STATIC_EXTENSION_ID = 'cjcgkaeffjeoljmjanapkpmdpbdceffm';
+  const callExtensionAPI = function (method) {
+    chrome.runtime.sendMessage(STATIC_EXTENSION_ID, {
+      methodName: method,
+    });
+  };
+  callExtensionAPI('acorda');
+
+  // receive message from extension
+  chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+    // draw message to screen document
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    document.body.appendChild(messageElement);
+  });
 }
